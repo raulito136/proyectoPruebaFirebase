2d4a11ed9c5aacccf995e7e86dc894d02a4ec541
1raulitosoyyo1 <1raulitosoyyo1@gmail.com>
2026-03-21 11:16:58 +0000

    Centrado de contenido en todas las tablas y botones

--- src/main/java/com/example/moviecollection/controller/AdminController.java
+++ src/main/java/com/example/moviecollection/controller/AdminController.java
@@ -8,6 +8,7 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
@@ -94,6 +95,20 @@ public class AdminController {
             } finally {
                 em.close();
             }
+        }
+    }
+
+    @FXML
+    void logout(ActionEvent event) {
+        try {
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Parent root = FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+            Scene scene = new Scene(root, 500, 400);
+            scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
